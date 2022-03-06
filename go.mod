8c0f9d05ecd5bedfdfdc7cc287eb4a2243c0fb2e
DevYmA <ymamarathunga@gmail.com>
2022-03-06 18:52:34 +0530

    added go cli user welcome feature

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module booking-app
+module go-cli
 
 go 1.17
--- main.go
+++ main.go
@@ -3,5 +3,10 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello World")
+
+	var applicationName = "Go CLI"
+	const messageCount = 30
+
+	fmt.Printf("Welcome to %v 🌝 \n", applicationName)
+	fmt.Println("Before rollout to hand shake enter your name 🚀")
 }
