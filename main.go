2e57357b170366a4a0fb0fd484f52b6adf8caec8
DevYmA <ymamarathunga@gmail.com>
2022-03-07 19:20:45 +0530

    added var types and user input

--- main.go
+++ main.go
@@ -4,9 +4,21 @@ import "fmt"
 
 func main() {
 
-	var applicationName = "Go CLI"
-	const messageCount = 30
+	var applicationName string = "Go CLI"
+	const messageCount int = 30
 
 	fmt.Printf("Welcome to %v 🌝 \n", applicationName)
 	fmt.Println("Before rollout to hand shake enter your name 🚀")
+
+	var userName string
+	var userAge int
+
+	fmt.Println("Please enter your good name here ")
+	fmt.Scan(&userName)
+
+	fmt.Println("Please enter your age ")
+	fmt.Scan(&userAge)
+
+	fmt.Printf("Your name is %v and Your age is %v \n", userName, userAge)
+	fmt.Println("We are looking details you have entered. ")
 }
