35e0553e231569614a9f0a74eccc55d1a00b7d32
DevYmA <ymamarathunga@gmail.com>
2022-03-10 19:38:04 +0530

    added details collection loop

--- main.go
+++ main.go
@@ -10,22 +10,43 @@ func main() {
 	var allGiftCount uint = 80
 	var givenGiftCount uint = 10
 
+	var winnerList []string
+
 	fmt.Printf("Welcome to %v 🌝 \n", applicationName)
-	fmt.Println("Before rollout to hand shake enter your name 🚀")
 
-	var userName string
-	var userAge int
+	for {
 
-	fmt.Println("Please enter your good name here ")
-	fmt.Scan(&userName)
+		var remainingGift uint = allGiftCount - givenGiftCount
+		fmt.Println("-------------------------------------------------------------")
 
-	fmt.Println("Please enter your age ")
-	fmt.Scan(&userAge)
+		fmt.Printf("We have %v availble gift. Hurry up \n \n", remainingGift)
 
-	fmt.Printf("Your name is %v and Your age is %v \n", userName, userAge)
-	fmt.Println("We are looking details you have entered. ")
+		fmt.Println("Before rollout to hand shake enter your details 🚀")
 
-	var remainingGift uint = allGiftCount - givenGiftCount
+		var firstName string
+		var lastName string
+		var email string
+		var age int
 
-	fmt.Printf("We have %v gift available right now. One of them are yours", remainingGift)
+		fmt.Println("Please enter your First Name :")
+		fmt.Scan(&firstName)
+
+		fmt.Println("Please enter your Last Name :")
+		fmt.Scan(&lastName)
+
+		fmt.Println("Please enter your Email :")
+		fmt.Scan(&email)
+
+		fmt.Println("Please enter your age ")
+		fmt.Scan(&age)
+
+		fmt.Println("Entered details are following ")
+		fmt.Printf("Your name is %v %v , email  %v and Your age is %v \n", firstName, lastName, email, age)
+
+		givenGiftCount = givenGiftCount + 1
+		winnerList = append(winnerList, email)
+
+		fmt.Printf("We have %v gift available right now. One of them are yours", remainingGift)
+		fmt.Printf("Gift Winners Emails %v \n ", winnerList)
+	}
 }
